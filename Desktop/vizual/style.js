188c584d4d03103a5d37f505b27c4888d0328c2f
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-04-19 23:31:05 +0300

    this my begginer style

---
+++ Desktop/vizual/style.js
@@ -0,0 +1,8 @@
+(function() {
+    const header = document.querySelector('h1');
+    header.style.color = 'blue'
+
+    document.querySelector('body').addEventListener('click', function() {
+        header.style.color = 'black'
+    })
+})();
